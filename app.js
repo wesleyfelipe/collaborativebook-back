f1b10e1d170e78eafbc790db955f27eb235b84e8
Wesley <wesleys.felipe@gmail.com>
2015-05-30 00:56:23 -0300

    Criando conexão com o Mongo

--- app.js
+++ app.js
@@ -1,69 +1,84 @@
 'use strict';
 
 /*
- * Express Dependencies
+ * Module dependencies
  */
-var express = require('express');
-var app = express();
-var port = 3000;
+var application_root = __dirname,
+express = require('express'), // Web framework
+path = require('path'), // file paths
+mongoose = require('mongoose'); // Acesso ao MongoDB
 
-/*
- * Use Handlebars for templating
- */
-var exphbs = require('express3-handlebars');
-var hbs;
 
-// For gzip compression
-app.use(express.compress());
-
-/*
- * Config for Production and Development
- */
-if (process.env.NODE_ENV === 'production') {
-    // Set the default layout and locate layouts and partials
-    app.engine('handlebars', exphbs({
-        defaultLayout: 'main',
-        layoutsDir: 'dist/views/layouts/',
-        partialsDir: 'dist/views/partials/'
-    }));
-
-    // Locate the views
-    app.set('views', __dirname + '/dist/views');
-    
-    // Locate the assets
-    app.use(express.static(__dirname + '/dist/assets'));
-
-} else {
-    app.engine('handlebars', exphbs({
-        // Default Layout and locate layouts and partials
-        defaultLayout: 'main',
-        layoutsDir: 'views/layouts/',
-        partialsDir: 'views/partials/'
-    }));
-
-    // Locate the views
-    app.set('views', __dirname + '/views');
-    
-    // Locate the assets
-    app.use(express.static(__dirname + '/assets'));
-}
-
-// Set Handlebars
-app.set('view engine', 'handlebars');
+//Conexão com o banco de dados 
+mongoose.connect( 'mongodb://localhost/colaborativebook' );
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Erro de conexão:'));
 
 
 
-/*
- * Routes
- */
-// Index Page
-app.get('/', function(request, response, next) {
-    response.render('index');
-});
-
-
-/*
- * Start it up
- */
-app.listen(process.env.PORT || port);
-console.log('Express started on port ' + port);
+//
+//
+///*
+// * Express Dependencies
+// */
+//var express = require('express');
+//var app = express();
+//var port = 3000;
+//
+///*
+// * Use Handlebars for templating
+// */
+//var exphbs = require('express3-handlebars');
+//var hbs;
+//
+//// For gzip compression
+//app.use(express.compress());
+//
+///*
+// * Config for Production and Development
+// */
+//if (process.env.NODE_ENV === 'production') {
+//	// Set the default layout and locate layouts and partials
+//	app.engine('handlebars', exphbs({
+//		defaultLayout : 'main',
+//		layoutsDir : 'dist/views/layouts/',
+//		partialsDir : 'dist/views/partials/'
+//	}));
+//
+//	// Locate the views
+//	app.set('views', __dirname + '/dist/views');
+//
+//	// Locate the assets
+//	app.use(express.static(__dirname + '/dist/assets'));
+//
+//} else {
+//	app.engine('handlebars', exphbs({
+//		// Default Layout and locate layouts and partials
+//		defaultLayout : 'main',
+//		layoutsDir : 'views/layouts/',
+//		partialsDir : 'views/partials/'
+//	}));
+//
+//	// Locate the views
+//	app.set('views', __dirname + '/views');
+//
+//	// Locate the assets
+//	app.use(express.static(__dirname + '/assets'));
+//}
+//
+//// Set Handlebars
+//app.set('view engine', 'handlebars');
+//
+///*
+// * Routes
+// */
+//// Index Page
+//app.get('/', function(request, response, next) {
+//	response.render('index');
+//});
+//
+///*
+// * Start it up
+// */
+//app.listen(process.env.PORT || port);
+//console.log('Express started on port ' + port);

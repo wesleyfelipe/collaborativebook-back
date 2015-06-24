2bf6ab50797f019632608571de3336ff86a0cd39
CarlosReinheimer <reinheimer.carlos@gmail.com>
2015-06-24 19:40:12 -0300

    Ajustado recurso capitulos

--- app/resources/capitulo.js
+++ app/resources/capitulo.js
@@ -3,20 +3,39 @@ var Capitulo = mongoose.model('Capitulo');
 
 exports.create = function (req, res) {
 
-    var capitulo = new Capitulo();
+    Livro.findOne({proprietario: req.user._id, _id: req.params.idLivro}, function (err, livro) {
 
-    capitulo.titulo = req.body.titulo;
-    capitulo.indice = req.body.indice;
-    capitulo.texto = req.body.texto;
-    capitulo.aprovado = req.body.aprovado;
-    capitulo.autor = req.body.autor;
-    capitulo.livro = req.params.idLivro;
-
-    capitulo.save(function (err) {
         if (err) {
             return res.send(err);
         }
-       res.json({message: 'Capítulo adicionado!', data: capitulo});
+
+        if (livro){
+
+            var capitulo = new Capitulo();
+
+            capitulo.titulo = req.body.titulo;
+            capitulo.indice = req.body.indice;
+            capitulo.texto = req.body.texto;
+            capitulo.aprovado = req.body.aprovado;
+            capitulo.autor = req.user._id;
+            capitulo.livro = req.params.idLivro;
+
+            capitulo.save(function (err) {
+                if (err) {
+                    return res.send(err);
+                }
+                res.json({message: 'Capítulo adicionado!', data: capitulo});
+            });
+
+        } else {
+
+            res.json({
+                "status": 401,
+                "message": "Este livro não pertence ao usuário logado."
+            });
+
+        }
+
     });
 
 };
